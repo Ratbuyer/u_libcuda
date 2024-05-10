0b93cf9d4aab3c2beb3ccc36c4fc194ac185746b
Eddy <m1662576219@gmail.com>
2024-05-09 21:16:42 -0400

    update

--- examples/overlap.cu
+++ examples/overlap.cu
@@ -23,17 +23,17 @@ __global__ void cuda_core_work(int *result)
 
   for (int i = 0; i < iteration; i++)
   {
-      sum = fma(1.0f, 1.0f, sum);
-      sum = fma(1.1f, 1.1f, sum);
-      sum = fma(1.2f, 1.2f, sum);
-      sum = fma(1.3f, 1.3f, sum);
-      sum = fma(1.4f, 1.4f, sum);
-      sum = fma(1.5f, 1.5f, sum);
-      sum = fma(1.6f, 1.6f, sum);
-      sum = fma(1.7f, 1.7f, sum);
-      sum = fma(1.8f, 1.8f, sum);
-      sum = fma(1.9f, 1.9f, sum);
-      sum = fma(1.0f, 1.0f, sum);
+    sum = fma(1.0f, 1.0f, sum);
+    sum = fma(1.1f, 1.1f, sum);
+    sum = fma(1.2f, 1.2f, sum);
+    sum = fma(1.3f, 1.3f, sum);
+    sum = fma(1.4f, 1.4f, sum);
+    sum = fma(1.5f, 1.5f, sum);
+    sum = fma(1.6f, 1.6f, sum);
+    sum = fma(1.7f, 1.7f, sum);
+    sum = fma(1.8f, 1.8f, sum);
+    sum = fma(1.9f, 1.9f, sum);
+    sum = fma(1.0f, 1.0f, sum);
   }
 
   result[0] = sum;
@@ -68,7 +68,7 @@ __global__ void tensor_core_work(int *result)
 
   asm volatile("wgmma.fence.sync.aligned; \n");
 
-  for (int i = 0; i < 100; i++)
+  for (int i = 0; i < iteration * 3; i++)
   {
     asm volatile("wgmma.mma_async.sync.aligned.m64n8k16.f16.f16.f16 "
                  "{%0, %1}, "
@@ -91,9 +91,75 @@ __global__ void tensor_core_work(int *result)
   result[0] = c[0] + c[1];
 }
 
-// __global__ void overlap(int *result)
-//{
-  //}
+__global__ void overlap(int *result)
+{
+  const int M = 64;
+  const int N = 8;
+  const int K = 16;
+
+  __align__(16) __shared__ half A_shared[M * K];
+  __align__(16) __shared__ half B_shared[K * N];
+
+  if (threadIdx.x == 0)
+  {
+    for (int i = 0; i < M * K; i++)
+    {
+      A_shared[i] = 0.01f;
+    }
+
+    for (int i = 0; i < K * N; i++)
+    {
+      B_shared[i] = 0.01f;
+    }
+  }
+
+  GmmaDescriptor desc_a = make_desc_a(A_shared);
+  GmmaDescriptor desc_b = make_desc_b(B_shared);
+
+  int c[2] = {};
+
+  asm volatile("wgmma.fence.sync.aligned; \n");
+
+  for (int i = 0; i < iteration * 3; i++)
+  {
+    asm volatile("wgmma.mma_async.sync.aligned.m64n8k16.f16.f16.f16 "
+                 "{%0, %1}, "
+                 "%2, %3, "
+                 "1, "
+                 "1, 1, "
+                 "0, 0;"
+                 : "+r"(c[0]), "+r"(c[1])
+                 : "l"(desc_a), "l"(desc_b));
+
+    // asm volatile("wgmma.fence.sync.aligned; \n");
+  }
+
+  asm volatile("wgmma.commit_group.sync.aligned; \n");
+
+  float sum = 0;
+
+  for (int i = 0; i < iteration; i++)
+  {
+    sum = fma(1.0f, 1.0f, sum);
+    sum = fma(1.1f, 1.1f, sum);
+    sum = fma(1.2f, 1.2f, sum);
+    sum = fma(1.3f, 1.3f, sum);
+    sum = fma(1.4f, 1.4f, sum);
+    sum = fma(1.5f, 1.5f, sum);
+    sum = fma(1.6f, 1.6f, sum);
+    sum = fma(1.7f, 1.7f, sum);
+    sum = fma(1.8f, 1.8f, sum);
+    sum = fma(1.9f, 1.9f, sum);
+    sum = fma(1.0f, 1.0f, sum);
+  }
+
+  asm volatile("wgmma.wait_group.sync.aligned 0; \n");
+
+  asm volatile("wgmma.fence.sync.aligned; \n");
+
+  result[0] = c[0] + c[1];
+  result[0] += reinterpret_cast<int &>(sum);
+}
 
 int main()
 {
