23ffc4330ba5eb1e138ca0f3b713d6f00fa33fe4
Eddy <m1662576219@gmail.com>
2024-05-09 21:13:09 -0400

    update

--- examples/overlap.cu
+++ examples/overlap.cu
@@ -68,7 +68,7 @@ __global__ void tensor_core_work(int *result)
 
   asm volatile("wgmma.fence.sync.aligned; \n");
 
-  for (int i = 0; i < iteration; i++)
+  for (int i = 0; i < iteration * 3; i++)
   {
     asm volatile("wgmma.mma_async.sync.aligned.m64n8k16.f16.f16.f16 "
                  "{%0, %1}, "
@@ -92,67 +92,8 @@ __global__ void tensor_core_work(int *result)
 }
 
 // __global__ void overlap(int *result)
-// {
-//   const int M = 64;
-//   const int N = 8;
-//   const int K = 16;
-
-//   __align__(16) __shared__ half A_shared[M * K];
-//   __align__(16) __shared__ half B_shared[K * N];
-
-//   GmmaDescriptor desc_a = make_desc_a(A_shared);
-//   GmmaDescriptor desc_b = make_desc_b(B_shared);
-
-//   int c[2] = {};
-
-//   asm volatile("wgmma.fence.sync.aligned; \n");
-
-//   for (int i = 0; i < 10; i++)
-//   {
-//     asm volatile("wgmma.mma_async.sync.aligned.m64n8k16.f16.f16.f16 "
-//                  "{%0, %1}, "
-//                  "%2, %3, "
-//                  "1, "
-//                  "1, 1, "
-//                  "0, 1;"
-//                  : "+r"(c[0]), "+r"(c[1])
-//                  : "l"(desc_a), "l"(desc_b));
-
-//     asm volatile("wgmma.fence.sync.aligned; \n");
-//   }
-
-//   asm volatile("wgmma.commit_group.sync.aligned; \n");
-
-//   asm volatile("wgmma.wait_group.sync.aligned 0; \n");
-
-//   asm volatile("wgmma.fence.sync.aligned; \n");
-
-//   result[0] = c[0] + c[1];
-
-//   int sum;
-//   int num_iterations = iteration;
-
-//   for (int i = 0; i < num_iterations; i++)
-//   {
-//     for (int j = 0; j < num_iterations; j++)
-//     {
-//       sum += i;
-//       sum -= i;
-//       sum += i;
-//       sum -= i;
-//       sum += i;
-//       sum -= i;
-//       sum += i;
-//       sum -= i;
-//       sum += i;
-//       sum -= i;
-//       sum += i;
-//     }
-//   }
-
-//   result[0] += sum;
-//   result[0] = 1;
-// }
+//{
+  //}
 
 int main()
 {
