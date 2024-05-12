54efa10af8a01c27e77cb228bc47d9887185df9b
Eddy <m1662576219@gmail.com>
2024-05-12 00:05:06 -0400

    update

--- examples/overlap.cu
+++ examples/overlap.cu
@@ -69,7 +69,7 @@ __global__ void tensor_core_work(int *result)
   result[0] = c[0] + c[1];
 }
 
-__global__ void overlap(int *result)
+__global__ void overlap_v2(int *result)
 {
   const int M = 64;
   const int N = 8;
@@ -97,7 +97,7 @@ __global__ void overlap(int *result)
                  "0, 0;"
                  : "+r"(c[0]), "+r"(c[1])
                  : "l"(desc_a), "l"(desc_b));
-    
+
     asm volatile("wgmma.commit_group.sync.aligned; \n");
 
     sum = fma(1.0f, 1.0f, sum);
@@ -106,11 +106,53 @@ __global__ void overlap(int *result)
     sum = fma(1.3f, 1.3f, sum);
   }
 
-  // asm volatile("wgmma.commit_group.sync.aligned; \n");
+  asm volatile("wgmma.wait_group.sync.aligned 0; \n");
 
-  // for (int i = 0; i < iteration; i++)
-  // {
-  // }
+  asm volatile("wgmma.fence.sync.aligned; \n");
+
+  result[0] = c[0] + c[1];
+  result[0] += reinterpret_cast<int &>(sum);
+}
+
+__global__ void overlap_v1(int *result)
+{
+  const int M = 64;
+  const int N = 8;
+  const int K = 16;
+
+  __align__(16) __shared__ half A_shared[M * K];
+  __align__(16) __shared__ half B_shared[K * N];
+
+  GmmaDescriptor desc_a = make_desc_a(A_shared);
+  GmmaDescriptor desc_b = make_desc_b(B_shared);
+
+  int c[2] = {};
+
+  float sum = 0;
+
+  asm volatile("wgmma.fence.sync.aligned; \n");
+
+  for (int i = 0; i < iteration; i++)
+  {
+    asm volatile("wgmma.mma_async.sync.aligned.m64n8k16.f16.f16.f16 "
+                 "{%0, %1}, "
+                 "%2, %3, "
+                 "1, "
+                 "1, 1, "
+                 "0, 0;"
+                 : "+r"(c[0]), "+r"(c[1])
+                 : "l"(desc_a), "l"(desc_b));
+  }
+
+  asm volatile("wgmma.commit_group.sync.aligned; \n");
+
+  for (int i = 0; i < iteration; i++)
+  {
+    sum = fma(1.0f, 1.0f, sum);
+    sum = fma(1.1f, 1.1f, sum);
+    sum = fma(1.2f, 1.2f, sum);
+    sum = fma(1.3f, 1.3f, sum);
+  }
 
   asm volatile("wgmma.wait_group.sync.aligned 0; \n");
 
@@ -162,7 +204,7 @@ int main()
 
   timer.start_timer();
 
-  overlap<<<blocks, threads_per_block>>>(d_result);
+  overlap_v1<<<blocks, threads_per_block>>>(d_result);
 
   timer.stop_timer();
 
